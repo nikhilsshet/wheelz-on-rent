a7f6874fcec6661651aae36be38a962001a2790f
nikhilsshet <nikhilsshet@gmail.com>
2025-07-23 15:44:52 +0530

    Initual commit: setup of frontend and backend folder structure

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Wheelz On Rent Backend")
+    })
+
+    fmt.Println("Server is running on port 8080")
+    http.ListenAndServe(":8080", r)
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Wheelz On Rent Frontend</h1>
+    </div>
+  );
+}
+export default App;
+
